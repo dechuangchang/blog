b19fe4639670e1d7518f333335e3ceff12e6fe7a
常德闯 <changdechuang@gmail.com>
2018-04-19 16:36:25 +0800

    CleanWebpackPlugin

--- webpack_config/plugins.config.js
+++ webpack_config/plugins.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const pluginsConfig = [
     new webpack.ProvidePlugin({
@@ -32,7 +32,17 @@ const pluginsConfig = [
         to:path.resolve(__dirname,'../output/iconfont')
     }]),
 ];
-
+if(process.env.npm_lifecycle_script.indexOf('production')>1){
+    console.log(path.resolve(__dirname,'../output/'))
+    pluginsConfig.push(
+        new CleanWebpackPlugin(
+            path.resolve(__dirname,'../output/'),{
+                root: path.resolve(__dirname,'../'),
+            }
+        )
+    )
+    console.log('CleanWebpackPlugin----->')
+}
 module.exports = pluginsConfig;
 console.log('pluginsConfig----->')
 
